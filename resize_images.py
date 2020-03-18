e79ed79914a60cd5481cbcfeb3f526c9f869f4df
中谷透大 <57211829+Tota-Nakatani@users.noreply.github.com>
2020-03-18 14:22:42 +0900

    Update resize_images.py

--- resize_images.py
+++ resize_images.py
@@ -1,4 +1,5 @@
 import os
+#コマンドライン引数でpyファイルを実行する際にはsysをインポート
 import sys
 from glob import glob
 
@@ -6,7 +7,8 @@ from PIL import Image
 
 
 def resize_images(images_dir, image_save_dir, image_size):
-
+    #os.mkdirとは違い中間ディレクトリーも作成できる
+    #exist_ok=True:存在していてもエラーを吐かない
     os.makedirs(image_save_dir, exist_ok=True)
 
     img_paths = glob(os.path.join(images_dir, '*'))
@@ -15,6 +17,8 @@ def resize_images(images_dir, image_save_dir, image_size):
         # resize
         image = Image.open(img_path)
         rgb_im = image.convert('RGB')
+        #Resize:新しい画像として生成する。アスペクト比は変化する。
+        #thumbnail:元の画像に変更を加える(破壊),アスペクト比は維持される。
         rgb_im.thumbnail([image_size,image_size])
 
         # make background
@@ -22,7 +26,9 @@ def resize_images(images_dir, image_save_dir, image_size):
         back_ground.paste(rgb_im)
 
         # make path
+        #os.path.bassename:ファイル名を取得して保存先にパスを通す。
         save_path = os.path.join(image_save_dir, os.path.basename(img_path))
+        #.までの文字列を取得
         end_index = save_path.rfind('.')
         save_path = save_path[0:end_index]+'.jpg'
         print('save',save_path)
